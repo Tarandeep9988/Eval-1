12c7a8f01c8494fe4badd21ddb102640826461f7
Tarandeep9988 <tarandeep65singh@gmail.com>
2024-10-22 13:48:49 +0530

    fixed navbar of index.html

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 const getStartedBtns = document.querySelectorAll('.get_started_btn');
+const subscribeBtn = document.getElementById('subscribe_btn');
 
 for (const btn of getStartedBtns) {
     btn.addEventListener('click', () => {
         window.location.href = './levels/levels.html';
     })
+}
+
+subscribeBtn.onclick = () => {
+    alert("Thanks for subscribing")
+}
+
+// Scrolling event
+window.onscroll = function() {
+    const ele = document.querySelector("header");
+    if (window.scrollY > 10) {
+        ele.classList.add('scrolled');
+        console.log("this");
+        
+    }
+    else {
+        ele.classList.remove('scrolled');
+    }
 }
