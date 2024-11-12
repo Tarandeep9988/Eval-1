ca0c8cf680f75339232b48407894ae1be4741c0b
Tarandeep9988 <tarandeep65singh@gmail.com>
2024-11-12 13:42:35 +0530

    fixed color issues in subscribe section

--- Eval-3/src/components/Levels/LevelPage.jsx
+++ Eval-3/src/components/Levels/LevelPage.jsx
@@ -5,36 +5,38 @@ import { BeginnerLevels, IntermediateLevels, AdvancedLevels } from './Data'
 function LevelPage() {
   return (
     <>
-    <div className={styles.parent_box}>
-            <h2 className={styles.sec_heading}>Beginner Levels</h2>
-            <div className={styles.section} id={styles.beginner_box}>
-                {
-                    BeginnerLevels.map((level, index) => {
-                        return <LevelCard key={index} levelIconSrc={level.icon} levelTitle={level.name} levelNumber={level.level} />
-                    })
-                }
+    <div className={styles.body}>
+        <div className={styles.parent_box}>
+                <h2 className={styles.sec_heading}>Beginner Levels</h2>
+                <div className={styles.section} id={styles.beginner_box}>
+                    {
+                        BeginnerLevels.map((level, index) => {
+                            return <LevelCard key={index} levelIconSrc={level.icon} levelTitle={level.name} levelNumber={level.level} />
+                        })
+                    }
+                </div>
             </div>
-        </div>
-    <div className={styles.parent_box}>
-            <h2 className={styles.sec_heading}>Intermediate Levels</h2>
-            <div className={styles.section} id={styles.intermediate_box}>
-                {
-                    IntermediateLevels.map((level, index) => {
-                        return <LevelCard key={index} levelIconSrc={level.icon} levelTitle={level.name} levelNumber={level.level} />
-                    })
-                }
+        <div className={styles.parent_box}>
+                <h2 className={styles.sec_heading}>Intermediate Levels</h2>
+                <div className={styles.section} id={styles.intermediate_box}>
+                    {
+                        IntermediateLevels.map((level, index) => {
+                            return <LevelCard key={index} levelIconSrc={level.icon} levelTitle={level.name} levelNumber={level.level} />
+                        })
+                    }
+                </div>
             </div>
-        </div>
-    <div className={styles.parent_box}>
-            <h2 className={styles.sec_heading}>Advanced Levels</h2>
-            <div className={styles.section} id={styles.advanced_box}>
-                {
-                    AdvancedLevels.map((level, index) => {
-                        return <LevelCard key={index} levelIconSrc={level.icon} levelTitle={level.name} levelNumber={level.level} />
-                    })
-                }
+        <div className={styles.parent_box}>
+                <h2 className={styles.sec_heading}>Advanced Levels</h2>
+                <div className={styles.section} id={styles.advanced_box}>
+                    {
+                        AdvancedLevels.map((level, index) => {
+                            return <LevelCard key={index} levelIconSrc={level.icon} levelTitle={level.name} levelNumber={level.level} />
+                        })
+                    }
+                </div>
             </div>
-        </div>
+    </div>
     </>
   )
 }
