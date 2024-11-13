b6a243de52afe4b54ef5260d0d3d3ce297ba4dfe
Tarandeep9988 <tarandeep65singh@gmail.com>
2024-11-13 09:49:20 +0530

    login logic enhanced

--- Eval-3/src/components/Home/EntrySection.jsx
+++ Eval-3/src/components/Home/EntrySection.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 function EntrySection() {
+  const navigate = useNavigate();
+
+  const handleGetStarted = () => {
+    if (localStorage.getItem('isLoggedIn') === 'true') {
+      navigate('/levels')
+    }
+    else {
+      alert('Please login to continue');
+      navigate('/login');
+    }
+    
+  };
+  
   return (
     <>
         <div className="main_section">
@@ -9,7 +23,7 @@ function EntrySection() {
                 <h2 className="main-section-heading">Master Your Typing Skills - Speed, Accuracy, and Fun in One Place!</h2>
                 <div className="main_child_box">
                     <Link to={'typingtest'}>Typing test</Link>
-                    <Link to={'levels'}>Get Started</Link>
+                    <button className='main_child_a' onClick={handleGetStarted}>Get Started</button>
                 </div>
             </div>
         </div>
